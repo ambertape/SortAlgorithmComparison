2f9d12bcd700bb280c7e920de2b3e61c95347637
ambertape <khek@ambertape.ru>
2023-02-28 17:20:02 +0300

    Test dialog.

--- sources/SortAlgorithmComparison/Utils/SortUtils.cs
+++ sources/SortAlgorithmComparison/Utils/SortUtils.cs
@@ -134,4 +134,32 @@ public static class SortUtils
         Swap(ref array[pivot], ref array[maxIndex]);
         return pivot;
     }
+
+    /// <summary>
+    /// Gets whether list is ordered.
+    /// </summary>
+    /// <param name="list">List.</param>
+    /// <param name="comparer">Comparer.</param>
+    /// <typeparam name="T">Type.</typeparam>
+    /// <returns>Returns ordered or not.</returns>
+    public static bool IsOrdered<T>(this IList<T> list, IComparer<T> comparer = null)
+    {
+        if (comparer == null)
+        {
+            comparer = Comparer<T>.Default;
+        }
+
+        if (list.Count > 1)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (comparer.Compare(list[i - 1], list[i]) > 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
--- sources/SortAlgorithmComparison/ViewModel/TestDialogViewModel.cs
+++ sources/SortAlgorithmComparison/ViewModel/TestDialogViewModel.cs
@@ -1,7 +1,13 @@
+using System.Collections.ObjectModel;
 using Microsoft.Extensions.Logging;
+using ReactiveUI.Fody.Helpers;
 using SortAlgorithmComparison.Algorithms.Interfaces;
+using SortAlgorithmComparison.Services;
+using SortAlgorithmComparison.Utils;
 using Waves.UI.Base.Attributes;
+using Waves.UI.Dialogs;
 using Waves.UI.Presentation;
+using Waves.UI.Services.Interfaces;
 
 namespace SortAlgorithmComparison.ViewModel;
 
@@ -9,23 +15,62 @@ namespace SortAlgorithmComparison.ViewModel;
 /// Test dialog view model.
 /// </summary>
 [WavesViewModel(typeof(TestDialogViewModel))]
-public class TestDialogViewModel : WavesParameterizedViewModelBase<ISortingAlgorithm>
+public class TestDialogViewModel : WavesDialogViewModelBase<ISortingAlgorithm, bool>
 {
+    private readonly DataGeneratorService _dataGeneratorService;
     private ISortingAlgorithm _algorithm;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="TestDialogViewModel"/> class.
     /// </summary>
+    /// <param name="navigationService">Navigation service.</param>
     /// <param name="logger">Logger.</param>
-    public TestDialogViewModel(ILogger<TestDialogViewModel> logger)
-        : base(logger)
+    /// <param name="dataGeneratorService">Data generator service.</param>
+    public TestDialogViewModel(
+        IWavesNavigationService navigationService,
+        ILogger<TestDialogViewModel> logger,
+        DataGeneratorService dataGeneratorService)
+        : base(navigationService, logger)
     {
+        _dataGeneratorService = dataGeneratorService;
     }
 
+    /// <summary>
+    /// Gets or sets algorithm name.
+    /// </summary>
+    [Reactive]
+    public string AlgorithmName { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether array order test successful.
+    /// </summary>
+    [Reactive]
+    public bool IsOrdered { get; set; }
+
+    /// <summary>
+    /// Gets or sets unsorted items.
+    /// </summary>
+    public ObservableCollection<int> UnsortedItems { get; set; }
+
+    /// <summary>
+    /// Gets or sets unsorted items.
+    /// </summary>
+    public ObservableCollection<int> SortedItems { get; set; }
+
     /// <inheritdoc />
-    public override Task Prepare(ISortingAlgorithm t)
+    public override async Task Prepare(ISortingAlgorithm t)
     {
         _algorithm = t;
-        return Task.CompletedTask;
+
+        AlgorithmName = _algorithm.Name;
+
+        var unsortedArray = await _dataGeneratorService.Generate(200);
+        UnsortedItems = new ObservableCollection<int>(unsortedArray);
+
+        var sortedArray = UnsortedItems.ToArray();
+        _algorithm.Sort(ref sortedArray);
+        SortedItems = new ObservableCollection<int>(sortedArray);
+
+        IsOrdered = sortedArray.IsOrdered();
     }
 }
